e3d2f3dbb782c20048739d107315cff076371f99
Екатерина Любимская <knorotova@bk.ru>
2022-09-05 19:08:49 +0200

    Задача 10

---
+++ Homework2/Task10/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int digit1 = number % 100;
+int digit = digit1 / 10;
+
+if (number > 99 && number < 1000)
+{
+Console.WriteLine (number); 
+Console.WriteLine (digit);
+}
+else
+{
+   Console.WriteLine ("Неверный ввод");
+}
