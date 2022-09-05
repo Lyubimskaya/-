a3d73b90d304d0e7818f9870c1f9612eaf5af158
Екатерина Любимская <knorotova@bk.ru>
2022-09-05 23:17:07 +0200

    -a

--- Homework2/Task10/Program.cs
+++ Homework2/Task10/Program.cs
@@ -5,17 +5,16 @@
 // 918 -> 1
 
 Console.WriteLine("Введите трехзначное число:");
-int number = Convert.ToInt32(Console.ReadLine());
+int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
 
 int digit1 = number % 100;
 int digit = digit1 / 10;
 
 if (number > 99 && number < 1000)
 {
-Console.WriteLine (number); 
 Console.WriteLine (digit);
 }
-else
+else 
 {
    Console.WriteLine ("Неверный ввод");
 }
