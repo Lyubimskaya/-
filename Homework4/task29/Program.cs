19510b10f2a54d94067e0f01caca02fba56eadcd
Екатерина Любимская <knorotova@bk.ru>
2022-09-15 15:49:17 +0200

    Задача 29

---
+++ Homework4/task29/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. Данные вводятся с консоли пользователем
+
+int[] array = new int[8];
+for(int i = 0; i <=7; i++)
+{
+Console.Write("Введите элемент " + (i+1) + ": ");
+array[i] = Convert.ToInt32(Console.ReadLine());
+}
+for(int i = 0; i <=7; i++)
+{
+    Console.Write(array[i] + " ");
+}
